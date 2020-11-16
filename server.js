3658ac6196ed14cc2ac17c2b7bec4e34a8931992
lmachens <leon.machens@googlemail.com>
2020-11-16 12:19:26 +0100

    Connect to db and get password by name

--- server.js
+++ server.js
@@ -1,16 +1,30 @@
+require("dotenv").config();
+
 const express = require("express");
+const { getPassword } = require("./lib/passwords");
+const { connect } = require("./lib/database");
 
 const app = express();
 const port = 3600;
 
-app.get("/api/passwords/wifi", (request, response) => {
-  response.send("WIFI is ASJKHSDGPFJDS");
+app.get("/api/passwords/:name", async (request, response) => {
+  const { name } = request.params;
+  const passwordValue = await getPassword(name);
+  response.send(passwordValue);
 });
 
 app.post("/api/passwords", (request, response) => {
   response.send("Under construction");
 });
 
-app.listen(port, () => {
-  console.log(`PW4U API listening at http://localhost:${port}`);
-});
+async function run() {
+  console.log("Connecting to database...");
+  await connect(process.env.MONGO_DB_URI, process.env.MONGO_DB_NAME);
+  console.log("Connected to database 🎉");
+
+  app.listen(port, () => {
+    console.log(`PW4U API listening at http://localhost:${port}`);
+  });
+}
+
+run();
