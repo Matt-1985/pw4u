bb14eb5641bc1a1d14d91096876ede0a11da459f
lmachens <leon.machens@googlemail.com>
2020-11-16 12:10:31 +0100

    Prepare POST

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ app.get("/api/passwords/wifi", (request, response) => {
   response.send("WIFI is ASJKHSDGPFJDS");
 });
 
+app.post("/api/passwords", (request, response) => {
+  response.send("Under construction");
+});
+
 app.listen(port, () => {
   console.log(`PW4U API listening at http://localhost:${port}`);
 });
